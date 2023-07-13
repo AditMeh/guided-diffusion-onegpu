d7e3ebd8719a9861f1edf2bec5c6c23e62ae7ea7
aditmeh <aditmeh123@gmail.com>
2023-07-13 18:20:45 -0400

    saved images

--- scripts/image_sample.py
+++ scripts/image_sample.py
@@ -8,9 +8,9 @@ import os
 
 import numpy as np
 import torch as th
-import torch.distributed as dist
+import torchvision
 
-from guided_diffusion import dist_util, logger
+from guided_diffusion import logger
 from guided_diffusion.script_util import (
     NUM_CLASSES,
     model_and_diffusion_defaults,
@@ -23,7 +23,7 @@ from guided_diffusion.script_util import (
 def main():
     args = create_argparser().parse_args()
 
-    dist_util.setup_dist()
+    # dist_util.setup_dist()
     logger.configure()
 
     logger.log("creating model and diffusion...")
@@ -31,9 +31,9 @@ def main():
         **args_to_dict(args, model_and_diffusion_defaults().keys())
     )
     model.load_state_dict(
-        dist_util.load_state_dict(args.model_path, map_location="cpu")
+        th.load(args.model_path, map_location="cpu")
     )
-    model.to(dist_util.dev())
+    model.to('cuda:0')
     if args.use_fp16:
         model.convert_to_fp16()
     model.eval()
@@ -45,7 +45,7 @@ def main():
         model_kwargs = {}
         if args.class_cond:
             classes = th.randint(
-                low=0, high=NUM_CLASSES, size=(args.batch_size,), device=dist_util.dev()
+                low=0, high=NUM_CLASSES, size=(args.batch_size,), device="cuda:0"
             )
             model_kwargs["y"] = classes
         sample_fn = (
@@ -57,19 +57,20 @@ def main():
             clip_denoised=args.clip_denoised,
             model_kwargs=model_kwargs,
         )
-        sample = ((sample + 1) * 127.5).clamp(0, 255).to(th.uint8)
+        sample = ((sample + 1) * 127.5).clamp(0, 255)
+        
+        torchvision.utils.save_image(sample, "test.png")
+        
+        sample = sample.to(th.uint8)
         sample = sample.permute(0, 2, 3, 1)
         sample = sample.contiguous()
-
-        gathered_samples = [th.zeros_like(sample) for _ in range(dist.get_world_size())]
-        dist.all_gather(gathered_samples, sample)  # gather not supported with NCCL
+        
+        gathered_samples = [th.zeros_like(sample)]
         all_images.extend([sample.cpu().numpy() for sample in gathered_samples])
         if args.class_cond:
-            gathered_labels = [
-                th.zeros_like(classes) for _ in range(dist.get_world_size())
-            ]
-            dist.all_gather(gathered_labels, classes)
+            gathered_labels = [th.zeros_like(classes)]
             all_labels.extend([labels.cpu().numpy() for labels in gathered_labels])
+
         logger.log(f"created {len(all_images) * args.batch_size} samples")
 
     arr = np.concatenate(all_images, axis=0)
@@ -77,23 +78,28 @@ def main():
     if args.class_cond:
         label_arr = np.concatenate(all_labels, axis=0)
         label_arr = label_arr[: args.num_samples]
-    if dist.get_rank() == 0:
-        shape_str = "x".join([str(x) for x in arr.shape])
-        out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
-        logger.log(f"saving to {out_path}")
-        if args.class_cond:
-            np.savez(out_path, arr, label_arr)
-        else:
-            np.savez(out_path, arr)
 
-    dist.barrier()
+    shape_str = "x".join([str(x) for x in arr.shape])
+    out_path = os.path.join(logger.get_dir(), f"samples_{shape_str}.npz")
+    logger.log(f"saving to {out_path}")
+    if args.class_cond:
+        np.savez(out_path, arr, label_arr)
+    else:
+        import imageio
+        
+        for i in range(arr.shape[0]):
+            print(np.unique(arr))
+            imageio.imsave(f'{i}.png', arr[i])
+            
+        np.savez(out_path, arr)
+
     logger.log("sampling complete")
 
 
 def create_argparser():
     defaults = dict(
         clip_denoised=True,
-        num_samples=10000,
+        num_samples=16,
         batch_size=16,
         use_ddim=False,
         model_path="",
